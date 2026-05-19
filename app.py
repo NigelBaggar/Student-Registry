e0a56dc0648515cf96d107cdc0fc7bbe3d693699
Nigel Nyariri <skybaggar@gmail.com>
2026-05-19 20:19:06 +0200

    Fix database URI for PostgreSQL on Render

--- app.py
+++ app.py
@@ -12,7 +12,10 @@ from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+uri = os.environ.get('SQLALCHEMY_DATABASE_URI')
+if uri and uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
